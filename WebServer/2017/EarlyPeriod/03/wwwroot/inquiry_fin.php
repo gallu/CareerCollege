0bf03c5de996e46a7d50228a82b7224d3dc87890
Michiaki Furusho <m.furusho+github@gmail.com>
2017-05-09 16:01:53 +0900

    第三回授業内容のcommit

---
+++ WebServer/2017/EarlyPeriod/03/wwwroot/inquiry_fin.php
@@ -0,0 +1,58 @@
+<?php
+// inquiry_fin.php
+
+// 入力された情報を取得
+/*
+$email = (string)@$_POST['email'];
+$email = (string)filter_input(INPUT_POST, 'email');
+*/
+$params = array(
+    'email', 'name', 'birthday', 'body'
+);
+$input_data = array();
+foreach($params  as  $p) {
+    $input_data[$p] = (string)@$_POST[$p];
+}
+var_dump($input_data);
+
+// validate(情報は正しい？)
+$error_detail = array(); // エラー情報格納用変数
+
+// 必須チェック
+$must_params = array('email', 'body');
+foreach($must_params  as  $p) {
+    if ('' === $input_data[$p]) {
+        // エラー処理
+        $error_detail["error_must_{$p}"] = true;
+    }
+}
+
+// 型チェック：email
+// XXX RFC非準拠のメアドはしらん！！
+if (false === filter_var($input_data['email'], FILTER_VALIDATE_EMAIL)) {
+    // エラー処理
+    $error_detail["error_format_email"] = true;
+}
+
+// 型チェック：日付
+if ('' !== $input_data['birthday']) {
+    if (false === strtotime($input_data['birthday'])) {
+        // エラー処理
+        $error_detail["error_format_birthday"] = true;
+    }
+}
+
+// エラー判定
+if (array() !== $error_detail) {
+    //
+var_dump($error_detail);
+    echo 'エラーがあったらしい！！';
+    exit;
+}
+// ダミー
+echo 'データのvalidateはOKでした！！';
+
+// 入力された情報をDBにinsert
+
+// 「ありがとう」Pageの出力
+
---
+++ WebServer/2017/EarlyPeriod/wwwroot/inquiry.php
@@ -0,0 +1,15 @@
+<?php
+// inquiry.php
+?>
+<html>
+
+<body>
+  <form action="./inquiry_fin.php" method="post">
+    emailアドレス(*):<input type="text" name="email"><br>
+    名前:<input type="text" name="name"><br>
+    誕生日:<input type="text" name="birthday"><br>
+    問い合わせ内容<textarea name="body"></textarea><br>
+    <button>問い合わせる</button>
+  </form>
+</body>
+</html>
