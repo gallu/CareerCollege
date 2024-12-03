a1e5960e0fde38bcae5f3a6cbdf9e6acf7553180
Michiaki Furusho <m.furusho@gmail.com>
2024-12-03 15:27:15 +0900

    授業内容

--- GameServer/2024/Sat12/gacha_monte_test.php
+++ GameServer/2024/Sat12/gacha_monte_test.php
@@ -1,12 +1,25 @@
 <?php  // gacha_monte_test.php
 
 require_once __DIR__ . "/gacha_base.php";
-require_once __DIR__ . "/gacha_1.php";
+// require_once __DIR__ . "/gacha_1.php";
+// require_once __DIR__ . "/gacha_2.php";
+// require_once __DIR__ . "/gacha_3.php";
+require_once __DIR__ . "/gacha_4.php";
 
-// XXX モンテカルロの実装
-
-
+// モンテカルロの実装
 $list = getGachaList();
-// var_dump($list);
-$card = gacha($list);
-var_dump($card);
+
+$total = [];
+const TIMES = 100000; // 試行回数
+for ($i = 0; $i < TIMES; ++$i) {
+    $card = gacha($list);
+    $total[$card["name"]] ??= 0; // Warning対策
+    $total[$card["name"]] ++;
+}
+// var_dump($total);
+ksort($total);
+
+foreach ($total as $k => $v) {
+    $p = $v / TIMES * 100;
+    echo "{$k}: {$p}% <br>\n";
+}
--- GameServer/2024/Sat34/form.php
+++ GameServer/2024/Sat34/form.php
@@ -1,5 +1,11 @@
 <?php  // form.php
 
-// echo "form";
-var_dump( $_GET );
-var_dump( $_POST );
+// var_dump( $_GET );
+// var_dump( $_POST );
+
+$hoge = $_POST["hoge"] ?? 'default';
+// var_dump($hoge);
+
+// XSS対策として、htmlspecialchars()はちゃんと書く！！
+echo "入力したのは", htmlspecialchars($hoge), "ですね。";
+
